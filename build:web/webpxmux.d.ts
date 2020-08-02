7227f68ed239d41567fcbcfa27e536906e82c900
Makito <sumimakito@hotmail.com>
2020-08-02 16:53:37 +0800

    Removed the redundant folder

--- build:web/webpxmux.d.ts
+++
@@ -1,6 +0,0 @@
-export = Module;
-declare function Module(Module: any, ...args: any[]): any;
-declare namespace Module {
-    export { Module };
-}
-declare function Module(Module: any, ...args: any[]): any;
