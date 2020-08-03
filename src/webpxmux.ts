21dea15e0abf0a964bd67518e73e7998065edb4c
aina-haruka <68340842+aina-haruka@users.noreply.github.com>
2020-08-03 21:43:09 +0800

    fully support of webp config (#1)

--- src/webpxmux.ts
+++ src/webpxmux.ts
@@ -34,6 +34,34 @@ interface WebPXMuxModule extends EmscriptenModule {
   encodeFrames: (ptr: Ptr) => Ptr;
   decodeWebP: (ptr: Ptr, size: number) => Ptr;
   encodeWebP: (ptr: Ptr) => Ptr;
+  encodeWebPWithConfig: (ptr: Ptr, config: Ptr) => Ptr;
+
+  newWebPConfig: () => Ptr;
+  setWebPConfigLossless: (ptr: Ptr, value: number) => void;
+  setWebPConfigQuality: (ptr: Ptr, value: number) => void;
+  setWebPConfigMethod: (ptr: Ptr, value: number) => void;
+  setWebPConfigTargetSize: (ptr: Ptr, value: number) => void;
+  setWebPConfigTargetPSNR: (ptr: Ptr, value: number) => void;
+  setWebPConfigSegments: (ptr: Ptr, value: number) => void;
+  setWebPConfigSNSStrength: (ptr: Ptr, value: number) => void;
+  setWebPConfigFilterStrength: (ptr: Ptr, value: number) => void;
+  setWebPConfigFilterSharpness: (ptr: Ptr, value: number) => void;
+  setWebPConfigFilterType: (ptr: Ptr, value: number) => void;
+  setWebPConfigAutoFilter: (ptr: Ptr, value: number) => void;
+  setWebPConfigAlphaCompression: (ptr: Ptr, value: number) => void;
+  setWebPConfigAlphaFiltering: (ptr: Ptr, value: number) => void;
+  setWebPConfigAlphaQuality: (ptr: Ptr, value: number) => void;
+  setWebPConfigPass: (ptr: Ptr, value: number) => void;
+  setWebPConfigPreprocessing: (ptr: Ptr, value: number) => void;
+  setWebPConfigPartitions: (ptr: Ptr, value: number) => void;
+  setWebPConfigPartitionsLimit: (ptr: Ptr, value: number) => void;
+  setWebPConfigEmulateJPEGSize: (ptr: Ptr, value: number) => void;
+  setWebPConfigThreadLevel: (ptr: Ptr, value: number) => void;
+  setWebPConfigLowMemory: (ptr: Ptr, value: number) => void;
+  setWebPConfigNearLossless: (ptr: Ptr, value: number) => void;
+  setWebPConfigExact: (ptr: Ptr, value: number) => void;
+  setWebPConfigUseDeltaPalette: (ptr: Ptr, value: number) => void;
+  setWebPConfigUseSharpYUV: (ptr: Ptr, value: number) => void;
 }
 
 const toUnsigned = (n: number) => n >>> 0;
@@ -64,6 +92,33 @@ export interface Bitmap {
   width: number;
   height: number;
   rgba: Uint32Array;
+}
+export interface WebPConfig {
+  lossless: number,
+  quality: number,
+  method: number,
+  target_size: number,
+  target_PSNR: number,
+  segments: number,
+  sns_strength: number,
+  filter_strength: number,
+  filter_sharpness: number,
+  filter_type: number,
+  autofilter: number,
+  alpha_compression: number,
+  alpha_filtering: number,
+  alpha_quality: number,
+  pass: number,
+  preprocessing: number,
+  partitions: number,
+  partition_limit: number,
+  emulate_jpeg_size: number,
+  thread_level: number,
+  low_memory: number,
+  near_lossless: number,
+  exact: number,
+  use_delta_palette: number,
+  use_sharp_yuv: number
 }
 
 class WebPXMux {
@@ -104,6 +159,111 @@ class WebPXMux {
       ]);
       this.Module!.encodeWebP = this.Module!.cwrap("encodeWebP", "number", [
         "number",
+      ]);
+      this.Module!.newWebPConfig = this.Module!.cwrap("new_webpwrapper_config", "number", []);
+      this.Module!.encodeWebPWithConfig = this.Module!.cwrap("encodeWebPWithConfig", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigLossless = this.Module!.cwrap("set_webp_config_lossless", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigQuality = this.Module!.cwrap("set_webp_config_quality", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigMethod = this.Module!.cwrap("set_webp_config_method", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigTargetSize = this.Module!.cwrap("set_webp_config_target_size", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigTargetPSNR = this.Module!.cwrap("set_webp_config_target_PSNR", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigSegments = this.Module!.cwrap("set_webp_config_segments", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigSNSStrength = this.Module!.cwrap("set_webp_config_sns_strength", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigFilterStrength = this.Module!.cwrap("set_webp_config_filter_strength", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigFilterSharpness = this.Module!.cwrap("set_webp_config_filter_sharpness", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigFilterType = this.Module!.cwrap("set_webp_config_filter_type", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigAutoFilter = this.Module!.cwrap("set_webp_config_autofilter", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigAlphaCompression = this.Module!.cwrap("set_webp_config_alpha_compression", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigAlphaFiltering = this.Module!.cwrap("set_webp_config_alpha_filtering", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigAlphaQuality = this.Module!.cwrap("set_webp_config_alpha_quality", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigPass = this.Module!.cwrap("set_webp_config_pass", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigPreprocessing = this.Module!.cwrap("set_webp_config_preprocessing", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigPartitions = this.Module!.cwrap("set_webp_config_partitions", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigPartitionsLimit = this.Module!.cwrap("set_webp_config_partition_limit", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigEmulateJPEGSize = this.Module!.cwrap("set_webp_config_emulate_jpeg_size", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigThreadLevel = this.Module!.cwrap("set_webp_config_thread_level", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigLowMemory = this.Module!.cwrap("set_webp_config_low_memory", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigNearLossless = this.Module!.cwrap("set_webp_config_near_lossless", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigExact = this.Module!.cwrap("set_webp_config_exact", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigUseDeltaPalette = this.Module!.cwrap("set_webp_config_use_delta_palette", "number", [
+        "number",
+        "number"
+      ]);
+      this.Module!.setWebPConfigUseSharpYUV = this.Module!.cwrap("set_webp_config_use_sharp_yuv", "number", [
+        "number",
+        "number"
       ]);
       this.SIZE_SIZE_T = toUnsigned(
         this.Module!.cwrap("sizeof_size_t", "number", [])()
@@ -178,6 +338,36 @@ class WebPXMux {
     return ptr;
   }
 
+  generateWebPConfig(config: WebPConfig): Ptr {
+    const ptr = this.Module!.newWebPConfig();
+    this.Module!.setWebPConfigLossless(ptr, config.lossless);
+    this.Module!.setWebPConfigQuality(ptr, config.quality);
+    this.Module!.setWebPConfigMethod(ptr, config.method);
+    this.Module!.setWebPConfigTargetSize(ptr, config.target_size);
+    this.Module!.setWebPConfigTargetPSNR(ptr, config.target_PSNR);
+    this.Module!.setWebPConfigSegments(ptr, config.segments);
+    this.Module!.setWebPConfigSNSStrength(ptr, config.sns_strength);
+    this.Module!.setWebPConfigFilterStrength(ptr, config.filter_strength);
+    this.Module!.setWebPConfigFilterSharpness(ptr, config.filter_sharpness);
+    this.Module!.setWebPConfigFilterType(ptr, config.filter_type);
+    this.Module!.setWebPConfigAutoFilter(ptr, config.autofilter);
+    this.Module!.setWebPConfigAlphaCompression(ptr, config.alpha_compression);
+    this.Module!.setWebPConfigAlphaFiltering(ptr, config.alpha_filtering);
+    this.Module!.setWebPConfigAlphaQuality(ptr, config.alpha_quality);
+    this.Module!.setWebPConfigPass(ptr, config.pass);
+    this.Module!.setWebPConfigPreprocessing(ptr, config.preprocessing);
+    this.Module!.setWebPConfigPartitions(ptr, config.partitions);
+    this.Module!.setWebPConfigPartitionsLimit(ptr, config.partition_limit);
+    this.Module!.setWebPConfigEmulateJPEGSize(ptr, config.emulate_jpeg_size);
+    this.Module!.setWebPConfigThreadLevel(ptr, config.thread_level);
+    this.Module!.setWebPConfigLowMemory(ptr, config.low_memory);
+    this.Module!.setWebPConfigNearLossless(ptr, config.near_lossless);
+    this.Module!.setWebPConfigExact(ptr, config.exact);
+    this.Module!.setWebPConfigUseDeltaPalette(ptr, config.use_delta_palette);
+    this.Module!.setWebPConfigUseSharpYUV(ptr, config.use_sharp_yuv);
+    return ptr;
+  }
+
   async decodeFrames(webPData: Uint8Array) {
     const size = webPData.length * webPData.BYTES_PER_ELEMENT;
     const ptr = this.Module!._malloc(size);
@@ -237,6 +427,38 @@ class WebPXMux {
       rgba,
     };
     return bitmap;
+  }
+  
+  async encodeWebPWithConfig(bitmap: Bitmap, config: WebPConfig) {
+    const frames: Frames = {
+      frameCount: 1,
+      width: bitmap.width,
+      height: bitmap.height,
+      loopCount: 0,
+      bgColor: 0,
+      frames: [
+        {
+          duration: 0,
+          isKeyframe: false,
+          rgba: bitmap.rgba,
+        },
+      ],
+    };
+
+    const bsPtr = this.copyFBSToHeap(frames);
+    const configPtr = this.generateWebPConfig(config);
+    const encodedPtr = this.Module!.encodeWebPWithConfig(bsPtr, configPtr);
+    this.Module!._free(bsPtr);
+    this.Module!._free(configPtr);
+    if (encodedPtr < 0) {
+      throw Error(ErrorMessages[encodedPtr]);
+    }
+    const size = this.getUnsigned(encodedPtr, this.SIZE_SIZE_T);
+    const u8a = new Uint8Array(size / SIZE_IU8).map((_, i) =>
+      this.getUnsigned(encodedPtr + SIZE_IU32 + i * SIZE_IU8, SIZE_IU8)
+    );
+    this.Module!._free(encodedPtr);
+    return u8a;
   }
 
   async encodeWebP(bitmap: Bitmap) {
